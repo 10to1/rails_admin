d6af04a7006554bb11aa8cec68f2827e9ef35dbd
Koen Van der Auwera <koen@atog.be>
2011-09-13 13:20:38 +0200

    add photo to list of image property names

--- lib/rails_admin/config/fields/factories/carrier_wave.rb
+++ lib/rails_admin/config/fields/factories/carrier_wave.rb
@@ -7,7 +7,7 @@ if defined?(::CarrierWave)
     register_factory do |parent, properties, fields|
       model = parent.abstract_model.model
       if model.kind_of?(CarrierWave::Mount) && model.uploaders.include?(properties[:name])
-        type = properties[:name] =~ /image|picture|thumb/ ? :carrier_wave_image : :carrier_wave_file
+        type = properties[:name] =~ /image|picture|thumb|photo/ ? :carrier_wave_image : :carrier_wave_file
         fields << RailsAdmin::Config::Fields::Types.load(type).new(parent, properties[:name], properties)
         true
       else
